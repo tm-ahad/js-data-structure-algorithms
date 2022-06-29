4010a620223387e00e8540ee0bda53368946eb7b
tm-ahad <tm.ahad.07@gmail.com>
2022-06-29 21:02:37 +0600

    selection sort done

--- src/sort/selectionsort.js
+++ src/sort/selectionsort.js
@@ -2,15 +2,13 @@
 const selection_sort = arr => {
    let sorted_arr = []
    let un_sorted_arr = [...arr]
-   let min = un_sorted_arr[0]
-   for (var el of un_sorted_arr) {
-      if (el < min){
-         min = el
-         delete un_sorted_arr[un_sorted_arr.indexOf(min)]
-         sorted_arr.push(min)
-      }
-      console.log(un_sorted_arr)
+   var minValue = un_sorted_arr[0]
+
+   for (let i of [...un_sorted_arr]) {
+      minValue = Math.min(...un_sorted_arr)
+      sorted_arr.push(minValue)
+      un_sorted_arr = un_sorted_arr.filter(val => val !== minValue)
    }
-   return sorted_arr 
+   return sorted_arr
 }
-console.log(selection_sort([2, 1, 3]))
+console.log(selection_sort([2, 1, 3, 5, 4, 8, 6, 7]))
